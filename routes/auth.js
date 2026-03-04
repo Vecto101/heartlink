dfb8878ecd7e962a5d5b165fdfd8de2292bb1579
Ningkhui Victor Riamei <ningkhuiriamei@gmail.com>
2026-03-04 23:35:29 +0530

    fix case sensitive model import

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 
-const User = require("../models/User");
+const User = require("../models/user");
 
 let currentUser = null;
 
